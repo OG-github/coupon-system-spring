00652e67dc10e3426601b94b8fc8253704356632
OG-github <ofek.gadot@mail.huji.ac.il>
2020-02-21 18:48:11 +0200

    JavaDoc

--- src/main/java/com/og/CouponSystemJB/service/LoginService.java
+++ src/main/java/com/og/CouponSystemJB/service/LoginService.java
@@ -27,18 +27,39 @@ import com.og.CouponSystemJB.rest.ClientSession;
 import com.og.CouponSystemJB.service.exception.LoginServiceException;
 
 /**
- *
+ * TODO
  */
 @Service
 public class LoginService {
 
+    /*----------------- Fields ---------------------------------------------------------------------------------------*/
+
+    /* TODO */
     private ApplicationContext context;
 
+    /* SQL repository for User Entity */
     private UserRepositorySql userRepository;
+
+    /* SQL repository for Admin Entity */
     private AdminRepositorySql adminRepository;
+
+    /* SQL repository for Company Entity */
     private CompanyRepositorySql companyRepository;
+
+    /* SQL repository for Customer Entity */
     private CustomerRepositorySql customerRepository;
 
+    /*----------------- Constructors ---------------------------------------------------------------------------------*/
+
+    /**
+     * Full Autowired constructor used automatically by Spring.
+     *
+     * @param context            TODO
+     * @param userRepository     JPA Repository for User (Autowired).
+     * @param adminRepository    JPA Repository for Admin (Autowired).
+     * @param companyRepository  JPA Repository for Company (Autowired).
+     * @param customerRepository JPA Repository for Customer (Autowired).
+     */
     @Autowired
     public LoginService(ApplicationContext context, UserRepositorySql userRepository,
                         AdminRepositorySql adminRepository,
@@ -51,50 +72,66 @@ public class LoginService {
         this.customerRepository = customerRepository;
     }
 
+    /*----------------- Methods / Functions -----------------------------------------------------------------------------*/
 
+    /**
+     * TODO
+     *
+     * @param email String email (unique to each User entity) to look up the User by.
+     * @param password String password for the User to gain access to a ClientSession.
+     * @return
+     * @throws LoginServiceException
+     * @throws CompanyException
+     * @throws CustomerException
+     */
     public ClientSession login(String email, String password)
             throws LoginServiceException, CompanyException, CustomerException {
-        Optional<User> user = this.userRepository.findByEmail(email);
-        if (!user.isPresent()) {
-            throw new LoginServiceException("Email is not registered ");
+        Optional<User> user = this.userRepository.findByEmail(email); // find user by email
+        if (!user.isPresent()) { // user by email not found in DB
+            throw new LoginServiceException(LoginServiceException.EMAIL_NOT_FOUND);
         }
-        if (!user.get().getPassword().equals(password)) {
-            throw new LoginServiceException("Password is incorrect ");
+        if (!user.get().getPassword().equals(password)) { // email found password doesn't match
+            throw new LoginServiceException(LoginServiceException.PASS_NOT_CORRECT);
         }
-        Client client = user.get().getClient();
-        ClientSession session = this.context.getBean(ClientSession.class);
-
+        Client client = user.get().getClient(); // get the Client from the User
+        ClientSession session = this.context.getBean(ClientSession.class); // get new session
+        // login admin
         if (client instanceof Admin) {
             Optional<Admin> admin = this.adminRepository.findById(client.getId());
             if (!admin.isPresent()) {
-                throw new LoginServiceException("Error finding corresponding Client ");
+                throw new LoginServiceException(LoginServiceException.CLIENT_NOT_FOUND);
             }
+            // get a service from appcontext
             AdminServiceImpl adminService = this.context.getBean(AdminServiceImpl.class);
-            adminService.setAdmin(admin.get());
-            session.setService(adminService);
+            adminService.setAdmin(admin.get()); // set specific Client for the service
+            session.setService(adminService); // set the service for this session
         }
+        // login company
         else if (client instanceof Company) {
             Optional<Company> company = this.companyRepository.findById(client.getId());
             if (!company.isPresent()) {
-                throw new LoginServiceException("Error finding corresponding Client ");
+                throw new LoginServiceException(LoginServiceException.CLIENT_NOT_FOUND);
             }
+            // get a service from appcontext
             CompanyServiceImpl companyService = this.context.getBean(CompanyServiceImpl.class);
-            companyService.setCompany(company.get());
-            session.setService(companyService);
+            companyService.setCompany(company.get()); // set specific Client for the service
+            session.setService(companyService); // set the service for this session
         }
+        // login customer
         else if (client instanceof Customer) {
             Optional<Customer> customer = this.customerRepository.findById(client.getId());
             if (!customer.isPresent()) {
-                throw new LoginServiceException("Error finding corresponding Client ");
+                throw new LoginServiceException(LoginServiceException.CLIENT_NOT_FOUND);
             }
+            // get a service from appcontext
             CustomerServiceImpl customerService = this.context.getBean(CustomerServiceImpl.class);
-            customerService.setCustomer(customer.get());
-            session.setService(customerService);
+            customerService.setCustomer(customer.get()); // set specific Client for the service
+            session.setService(customerService);  // set the service for this session
         }
-        else {
-            throw new LoginServiceException("Invalid client ");
+        else { // invalid type of client
+            throw new LoginServiceException(LoginServiceException.INVALID_CLIENT);
         }
-        session.accessed();
+        session.accessed(); // reset last time session was accessed
         return session;
     }
 
--- src/main/java/com/og/CouponSystemJB/service/exception/LoginServiceException.java
+++ src/main/java/com/og/CouponSystemJB/service/exception/LoginServiceException.java
@@ -2,6 +2,15 @@ package com.og.CouponSystemJB.service.exception;
 
 public class LoginServiceException extends Exception {
 
+    public static final String EMAIL_NOT_FOUND = "Email is not registered ";
+
+    public static final String PASS_NOT_CORRECT = "Password is incorrect ";
+
+    public static final String CLIENT_NOT_FOUND = "Error finding corresponding Client ";
+
+    public static final String INVALID_CLIENT = "Error finding corresponding Client ";
+
+
     public LoginServiceException(String msg){
         super(msg);
     }
